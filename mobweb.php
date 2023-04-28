a5c3b3a345137e88d5cc1c3cdc892c4a22d0ec03
Zenggir <132053436+Zenggir@users.noreply.github.com>
2023-04-28 15:22:04 +0600

    Add files via upload

---
+++ mobweb.php
@@ -0,0 +1,70 @@
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Document</title>
+  <link rel="stylesheet" href="ertegiler.css">
+</head>
+<body>
+<div id="page_container">
+  
+	<div id="main_content">
+
+		<div class="front face">
+    		
+    		<!-- ALL THE SITE CONTENT GOES HERE -->
+    		
+    		<header class="clearfix">
+	
+				<nav class="clearfix">
+					<a id="menu-toggle" class="anchor-link" href="#mobile-nav">MENU</a>
+					<ul>
+          <li><a href="#">Мазмұн</a></li>
+			        	<li><a href="kirispe.php">Кіріспе</a></li>	
+			        	<li><a href="adister.php">Тексеру әдістері</a></li>
+			        	<li><a href="quraldar.php">Тексеру құралдары</a></li>
+			        	<li><a href="natige.php">Нәтижелерін талдау</a></li>
+			        	<li><a href="strt.php">Тексеру стратегиялары</a></li>
+                <li><a href="mobweb.php">Мобильді және веб тестілеу</a></li>
+                <li><a href="game.php">Ойын сынағы</a></li>
+                <li><a href="naqty.php">Нақты жобалар</a></li>
+			        	<li><a href="index.php">GALAM</a></li>
+			        </ul>
+				</nav>
+			
+				<h1>Мобильді және веб тестілеу</h1>
+
+			</header>
+			
+			
+			<article class="clearfix">
+    
+<h5><a href=""> VII. Мобильді және веб тестілеу </a> </h5>
+<ul>
+<li>Мобильді қосымшаларды тестілеудің ерекшеліктері
+<li>Мобильді қолданбаларды тестілеу құралдары мен әдістері
+<li>Веб қолданбалы тестілеу
+
+<li>Веб-қосымшаларды тестілеудің ерекшеліктері
+<li>Веб-қосымшаларды сынау құралдары мен әдістері</li></Ul>
+
+			<p>
+
+            </p>	
+			
+			</article>
+			
+			
+	
+  		
+  		</div><!-- END .front -->
+  		
+  		
+	</div><!-- END #main_content -->
+
+</div><!-- END #page_container -->
+</body>
+</html>
